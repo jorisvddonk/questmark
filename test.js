455afc588bb1dada38ec4f53e89a128b0b1d24dd
Joris van de Donk <jorisvddonk@gmail.com>
2015-03-18 21:33:38 +0100

    Use markdownIt to parse questmark header w/ inlines!

--- test.js
+++ test.js
@@ -1,42 +1,49 @@
-var marked = require('marked');
 var MarkdownIt = require('markdown-it');
 var markdownIt = new MarkdownIt();
 var fs = require('fs');
 var util = require('util');
 var _ = require('lodash');
+var repl = require('repl');
 
-var ill = new marked.InlineLexer();
+var program = require('commander');
+
+program
+  .version('0.0.1')
+  .option('-r, --repl', 'Spawn REPL')
+  .parse(process.argv);
 
 var parseMD = function(text) {
-  var lexer = new marked.Lexer();
-  var tokens = lexer.lex(text);
+  var tokens = markdownIt.parse(text);
   return tokens;
 }
 
 var md_src = fs.readFileSync('spec.md').toString();
-var tokens = console.log(util.inspect(parseMD(md_src)));
-console.log(util.inspect(ill.lex(tokens)));
-
+var X = parseMD(md_src);
+console.log(util.inspect(X));
 
 var getTokensInHeader = function(tokens, headerToSearch) {
-  return _.reduce(tokens, function(memo, token){
-    if (token.type === 'heading') {
-      if (token.text !== headerToSearch) {
-        memo.foundInfoHeader = false;
-      }
+  return _.reduce(tokens, function(memo, token, index){
+    if (token.type === 'heading_open') {
+      memo.isParsingHeader = true;
+      memo.foundHeader = false;
     }
 
-    if (memo.foundInfoHeader) {
+    if (memo.foundHeader && !memo.isParsingHeader) {
       memo.tokensInHeader.push(token);
     }
 
-    if (token.type === 'heading') {
-      if (token.text === headerToSearch) {
-        memo.foundInfoHeader = true;
+    if (token.type === 'heading_close') {
+      memo.isParsingHeader = false;
+    }
+
+    if (memo.isParsingHeader && token.type === 'inline') {
+      if (token.content === headerToSearch) {
+        memo.foundHeader = true;
       }
     }
+
     return memo;
-  }, {foundInfoheader: false, tokensInHeader: []}).tokensInHeader;
+  }, {isParsingHeader: false, foundHeader: false, tokensInHeader: []}).tokensInHeader;
 }
 var getTokensInInfoHeader = function(tokens) {
   return getTokensInHeader(tokens, 'QUESTMARK-OPTIONS-HEADER');
@@ -44,13 +51,19 @@ var getTokensInInfoHeader = function(tokens) {
 
 var getQuestmarkOptions = function(tokens){
   var defaultQuestmarkOptions = {'hamster': 'kaasbal'};
+  var codeBlockInlineTokens = _.filter(_.flatten(_.pluck(_.filter(getTokensInInfoHeader(tokens), function(token) {
+    return token.type === 'inline';
+  }), 'children')), function(inline_token) {return inline_token.type === 'code_inline'});
+  console.log("XXX")
+  console.log(codeBlockInlineTokens)
   var codeBlockTokens = _.filter(getTokensInInfoHeader(tokens), function(token) {
-    return token.type === 'code';
+    return token.type === 'code_block';
   });
-  var codeJSONs = _.map(codeBlockTokens, function(codeBlockToken){
+  var allCodeblockTokens = codeBlockInlineTokens.concat(codeBlockTokens);
+  var codeJSONs = _.map(allCodeblockTokens, function(codeBlockToken){
     var retval = {};
     try {
-      retval = JSON.parse(codeBlockToken.text);
+      retval = JSON.parse(codeBlockToken.content);
     } catch (e) {}
     return retval;
   });
@@ -59,5 +72,16 @@ var getQuestmarkOptions = function(tokens){
   }, defaultQuestmarkOptions);
 }
 
+console.log(getQuestmarkOptions(X));
 
-console.log(getQuestmarkOptions(tokens));
+if (program.repl) {
+  var REPL = repl.start({
+    "useGlobal": true,
+    "useColors": true
+  });
+  REPL.context.parseMD = parseMD;
+  REPL.context.getTokensInInfoHeader = getTokensInInfoHeader;
+  REPL.context.getTokensInHeader = getTokensInHeader;
+  REPL.context.lodash = _;
+  REPL.context.X = X;
+}
