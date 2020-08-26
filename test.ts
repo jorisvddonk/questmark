f9176c6e8de74b415570e2a01f5ac885d6c3219a
joris <jorisvddonk@gmail.com>
2020-08-27 00:12:08 +0200

    add ability to load alternate .md file

--- test.ts
+++ test.ts
@@ -6,9 +6,10 @@ program
   .version('0.0.1')
   .option('-c, --clear', 'Clear console on each state')
   .option('-d, --debugging', 'Show debug info on console')
+  .option('--input <path>', "Load source .md file", "readme.md")
   .parse(process.argv);
 
-const md_src = fs.readFileSync('readme.md').toString();
+const md_src = fs.readFileSync(program.input).toString();
 const engine = new QuestmarkEngine(md_src, program.clear, program.debugging, {
   "test": () => {
     console.log("TEST FUNCTION INVOKED!!");
