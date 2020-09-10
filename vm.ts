ed551cb1ffb096b8f92a46719ec03647f758053d
joris <jorisvddonk@gmail.com>
2020-09-10 22:41:07 +0200

    add jgz instruction (jump if greater than zero)

--- vm.ts
+++ vm.ts
@@ -128,6 +128,13 @@ export const std_functions: Functions = {
     stack.push(retval);
     return retval;
   },
+  "jgz": (stack: Stack, context: Context, vm: VM) => { // jump (skip next instruction) if stack.pop() is greater than zero
+    const [num1] = getStackParams("jgz", ["number"], stack) as [number];
+    if (num1 > 0) {
+      vm.programCounter += 2; // it would normally already jump one, but since we modify it won't and we have to jump over ourselves
+}
+    return null;
+  },
 }
 
 export class VM {
