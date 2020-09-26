8d6cbadf73c7cc35d3384a31b1680bee9c0e9bee
Joris van de Donk <jorisvddonk@gmail.com>
2020-09-26 16:27:50 +0200

    fix --output param of cli, and add --no-run option

--- src/cli.ts
+++ src/cli.ts
@@ -3,12 +3,14 @@ import program from "commander";
 import { parseMarkdown } from "./parseMarkdown";
 import { Choice, QuestVM } from "./QuestVM";
 import inquirer from "inquirer";
+import { TzoVMState } from "tzo";
 
 program
   .version('0.0.1')
   .option('-c, --clear', 'Clear console on each state')
   .option('--input <path>', "Load source .md or .json file", "examples/cookieStore.md")
   .option('--output <path>', "Emit VMState .json file", "out.json")
+  .option('--no-run', "Do not actually load and run the VM; just parse input and (optionally) emit output")
   .parse(process.argv);
 
 const input_file = fs.readFileSync(program.input).toString();
@@ -25,14 +27,20 @@ const vm = new QuestVM(body => process.stdout.write(`${body}`), (choices: Choice
 }, {test: () => {
   console.log("Test function called!");
 }});
+
+let vmState: TzoVMState = undefined;
+
 if (program.input.endsWith(".json")) {
-  vm.loadVMState(JSON.parse(input_file as any));
-  vm.run();
+  vmState = JSON.parse(input_file) as TzoVMState;
 } else if (program.input.endsWith(".md")) {
-  const vmState = parseMarkdown(input_file).qvmState;
-  if (program.out) {
-    fs.writeFileSync(program.out, JSON.stringify(vmState, null, 2));
-  }
+  vmState = parseMarkdown(input_file).qvmState;
+}
+
+if (program.output) {
+  fs.writeFileSync(program.output, JSON.stringify(vmState, null, 2));
+}
+
+if (program.run) {
   vm.loadVMState(vmState);
   vm.run();
 }
